26151c243923efa6e39ad48e256ab64bf980fd69
NoemiC97 <70523761@certus.edu.pe>
2022-06-03 00:43:47 -1200

    version2.4

--- trabajo3.js
+++ trabajo3.js
@@ -1,3 +1,4 @@
+document.write("Una empresa de llamadas telefonicas, necesita un programa que le calcule el costo por llamada. Debe tener en cuenta:<br>A Fijo: 0.20 centimos por minuto<br>A Claro: 0.40 centimos por minuto<br>A Movistar: 0.60 centimos por minuto<br>A Entel: 1 sol el minuto<br>");
 
 let ofijo=0.20;
 let oclaro=0.40;
@@ -13,13 +14,13 @@ let cclaro=parseFloat(minhablado*oclaro);
 let cmovistar=parseFloat(minhablado*omovistar);
 let centel=parseFloat(minhablado*oentel);
 if(operador==1){
-    document.write(`El costo de la llamada es: S/ ${cfijo}`);
+    document.write(`<br>El costo de la llamada por ${minhablado} minutos a fijo es: S/ ${cfijo}`);
 }else if(operador==2){
-    document.write(`El costo de la llamada es: S/ ${cclaro}`);
+    document.write(`<br>El costo de la llamada por ${minhablado} minutos a claro es: S/ ${cclaro}`);
 }else if(operador==3){
-   document.write(`El costo de la llamada es: S/ ${cmovistar}`);
+   document.write(`<br>El costo de la llamada por ${minhablado} minutos a movistar es: S/ ${cmovistar}`);
 }else if(operador==4){
-    document.write(`El costo de la llamada es: S/ ${centel}`);
+    document.write(`<br>El costo de la llamada por ${minhablado} minutos a entel es: S/ ${centel}`);
 }else {
     alert("No se verifica el operador");
 }
