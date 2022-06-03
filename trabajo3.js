d0d3e70f8647b687270d9c610f2d2b7682757d60
NoemiC97 <70523761@certus.edu.pe>
2022-06-03 00:26:02 -1200

    version2.3

--- trabajo3.js
+++ trabajo3.js
@@ -1,16 +1,17 @@
-let fijo=0.20;
-let claro=0.40;
-let movistar=0.60;
-let entel=1.00;
+
+let ofijo=0.20;
+let oclaro=0.40;
+let omovistar=0.60;
+let oentel=1.00;
 
 let operador=prompt("¿A que operador te comunicaste? \n1.Fijo \n2.Claro \n3.Movistar\n4.Entel");
 let minhablado=parseInt(prompt(`¿Cuantos minutos tardò la llamada?`));
 
 
-let cfijo=parseFloat(minhablado*fijo);
-let cclaro=parseFloat(minhablado*claro);
-let cmovistar=parseFloat(minhablado*movistar);
-let centel=parseFloat(minhablado*entel);
+let cfijo=parseFloat(minhablado*ofijo);
+let cclaro=parseFloat(minhablado*oclaro);
+let cmovistar=parseFloat(minhablado*omovistar);
+let centel=parseFloat(minhablado*oentel);
 if(operador==1){
     document.write(`El costo de la llamada es: S/ ${cfijo}`);
 }else if(operador==2){
